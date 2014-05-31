0d8907271b400c60c2b43069421cf740a004c74a
codePONPON <codeponpon@gmail.com>
2014-05-31 12:17:34 +0700

    fix connect db

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-  $host     = "localhost";
-  $username = "root";
-  $password = "root";
+  $host     = "128.199.230.16";
+  $username = "goldflower";
+  $password = "goldflower";
   $db = mysql_connect($host,$username,$password);
