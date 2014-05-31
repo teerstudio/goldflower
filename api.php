2ce213f313f19245be20d89077da9b9a2defb752
codePONPON <codeponpon@gmail.com>
2014-05-31 11:59:41 +0700

    defined action

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+  $action = isset($_GET['action']) ? $_GET['action'] : isset($_POST['action']) ? $_POST['action'];
+
+  if($action == 'add'){
+
+  }
+  elseif($action == 'show'){
+
+  }
+  elseif($action == 'update'){
+
+  }
+  elseif($action == 'delete'){
+
+  }
+?>
