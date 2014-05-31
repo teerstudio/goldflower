05e2ef9ea6ee968c34e78126d4631fa5e3549b09
codePONPON <codeponpon@gmail.com>
2014-05-31 16:08:42 +0700

    add comment

--- detail.php
+++ detail.php
@@ -21,6 +21,13 @@
     <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </head>
   <body>
+  <?php
+    if(isset($_POST['message'])){
+      $postname = isset($_POST['postname']) ? $_POST['postname'] : 'guest';
+      $insert_sql = 'INSERT INTO tb_comment (message, postname, date) VALUES ("'.$_POST['message'].'", "'.$postname.'", "'.date( 'Y-m-d H:i:s', time()).'")';
+      mysql_query($insert_sql) or die(mysql_error());
+    }
+  ?>
     <div id="wrap">
       <?php
         $sql = 'SELECT * FROM tb_profile WHERE id = '.$_GET['pid'];
@@ -42,6 +49,28 @@
           <div class="row">
             <div class="col-lg-3 col-md-3">Detail</div>
             <div class="col-lg-9 col-md-9"><?php echo $result['detail']; ?></div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-3 col-md-3">Comment</div>
+            <div class="col-lg-9 col-md-9">
+            <form action="detail.php" method="post">
+              <textarea name="message" cols="100" rows="10"></textarea>
+            </form>
+            </div>
+          </div>
+
+          <div class="row">
+            <?php
+              $sql = 'SELECT * FROM tb_comment WHERE profile_id = '.$_GET['pid'];
+              $query = mysql_query($sql) or die(mysql_error());
+              while ($result = mysql_fetch_array($query)) {
+                echo '<div class="col-lg-12 col-md-12">';
+                echo '<p>'.$result['postname'] . ' ' . $result['date'] . '</p>';
+                echo $result['message'];
+                echo '</div>';
+              }
+            ?>
           </div>
         </div>
       </div>
