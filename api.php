60e00701360f5e89ea00299b57eec212dd597fd7
codePONPON <codeponpon@gmail.com>
2014-05-31 12:36:12 +0700

    add defualt api to show list

--- api.php
+++ api.php
@@ -1,16 +1,19 @@
 <?php
   include('config.php');
   $action = isset($_GET['action']) ? $_GET['action'] : isset($_POST['action']) ? $_POST['action'] : null;
+  $get = isset($_GET['get']) ? $_GET['get'] : isset($_POST['get']) ? $_POST['get'] : null;
   $response = array('data' => array());
 
   //  action=add
   if($action == 'add'){
-    $kama
 
   }
   //  action=show
   elseif($action == 'show'){
-
+    if($get == 'list'){
+      $query = '';
+      $db = mysql_query($query);
+    }
   }
   //  action=update
   elseif($action == 'update'){
