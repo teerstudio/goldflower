46a5d061bc4bedbab766c5174cc9ec9a330350f3
codePONPON <codeponpon@gmail.com>
2014-05-31 12:26:29 +0700

    add README

---
+++ README.md
@@ -0,0 +1,7 @@
+GOLDFLOWER API
+===================
+
+ADD - http://128.199.230.16/goldflower/api.php?action=add
+SHOW - http://128.199.230.16/goldflower/api.php?action=show
+UPDATE - http://128.199.230.16/goldflower/api.php?action=update
+DELETE - http://128.199.230.16/goldflower/api.php?action=delete
--- api.php
+++ api.php
@@ -2,15 +2,20 @@
   include('config.php');
   $action = isset($_GET['action']) ? $_GET['action'] : isset($_POST['action']) ? $_POST['action'] : null;
   $response = array('data' => array());
+
+  //  action=add
   if($action == 'add'){
 
   }
+  //  action=show
   elseif($action == 'show'){
 
   }
+  //  action=update
   elseif($action == 'update'){
 
   }
+  //  action=delete
   elseif($action == 'delete'){
 
   }
